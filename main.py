1c776e394f38b201c36b14d2debf698bbf611c2e
DevinJASloan <69693529+DevinJASloan@users.noreply.github.com>
2021-04-26 23:48:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import threading
 import requests
+import queue
+import re
+
+success = []
 
 count = input("How many threads would you like to use? ")
 
 url = 'https://172.16.20.100:9443/samlsso?spEntityID=travelocity.com'
 
-user = {
-    'Authorization': 'Basic <Base64Encoded[admin:admin]>',
-}
+header_data = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
+
+user = {'Authorization': 'Basic <Base64Encoded[admin:admin]>'}
 
 
 def do_request():
-    response = requests.post(url, data=user, verify=False)
+    response = requests.post(url, headers=header_data, data=user, verify=False)
     print(response)
 
 
